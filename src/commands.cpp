367bad43b5fcd61cd03246fe336a47826fb5d548
anirudhaiysola <anirudhisola1011@gmail.com>
2026-04-05 19:03:27 -0500

    codecrafters submit [skip ci]

--- src/commands.cpp
+++ src/commands.cpp
@@ -334,7 +334,7 @@ static std::unordered_map<std::string, CommandHandler> command_map = []()
 
         if (parsed[2] == "0-0")
         {
-            send(client_fd, "-ERR The ID specified in XADD must be greater than 0-0\r\n", 55, 0);
+            send(client_fd, "-ERR The ID specified in XADD must be greater than 0-0\r\n", 56, 0);
             return;
         }
         if (!stream.empty())
