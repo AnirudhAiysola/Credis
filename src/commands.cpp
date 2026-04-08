527893d88551019dc679e191e1e7ee9d9992cdca
anirudhaiysola <anirudhisola1011@gmail.com>
2026-04-08 10:01:59 -0500

    codecrafters submit [skip ci]

--- src/commands.cpp
+++ src/commands.cpp
@@ -730,7 +730,8 @@ static std::unordered_map<std::string, CommandHandler> command_map = []()
     };
     m["PSYNC"] = [](int client_fd, std::vector<std::string> &parsed)
     {
-        send(client_fd, "+FULLRESYNC <8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb> 0\r\n", 52, 0);
+        std::string response = "+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n";
+        send(client_fd, response.c_str(), response.size(), 0);
     };
 
     return m;
