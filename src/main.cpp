8b4ac30e033d87d06e9be5a1a761316eee0a2b8b
anirudhaiysola <anirudhisola1011@gmail.com>
2026-04-07 22:18:40 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,17 @@
 
 int main(int argc, char **argv)
 {
-  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
+  int port = 6379;
+  for (int i = 1; i < argc; i++)
+  {
+    if (std::string(argv[i]) == "--port" && i + 1 < argc)
+    {
+      port = std::stoi(argv[i + 1]);
+    }
+  }
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0)
@@ -28,8 +36,6 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  // Since the tester restarts your program quite often, setting SO_REUSEADDR
-  // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0)
   {
@@ -40,11 +46,11 @@ int main(int argc, char **argv)
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
+  server_addr.sin_port = htons(port);
 
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) != 0)
   {
-    std::cerr << "Failed to bind to port 6379\n";
+    std::cerr << "Failed to bind to port " << port << "\n";
     return 1;
   }
 
@@ -58,12 +64,9 @@ int main(int argc, char **argv)
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
   std::cout << "Waiting for a client to connect...\n";
-
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
   int client_fd;
-
   while ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&client_addr_len)) >= 0)
   {
     std::thread t(handle_client, client_fd);
@@ -71,6 +74,5 @@ int main(int argc, char **argv)
   }
 
   close(server_fd);
-
   return 0;
 }
